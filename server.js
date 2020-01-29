6c2f6ef3bb59878940156b736a05687a62f18274
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-01-28 23:07:44 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+
+/**
+ * Midelware
+ */
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "pug");
+
+app.get("/", function(req, res) {
+	res.render("index", {
+		title: "Home - express",
+		message: "Hello there!",
+		menu: [
+			{ link: "/", text: "Home", active: "is-active" },
+			{ link: "/about", text: "About" }
+		]
+	});
+});
+
+app.get("/about", function(req, res) {
+	res.render("about", {
+		title: "About - express",
+		message: "Hello there!",
+		menu: [
+			{ link: "/", text: "Home" },
+			{ link: "/about", text: "About", active: "is-active" }
+		]
+	});
+});
+
+app.get("/foo", function(req, res) {
+	const response = {
+		name: "Fulanito",
+		edad: 33,
+		url: req.url
+	};
+	res.send(response);
+});
+
+app.listen(3000, () => {
+	console.log("Puerto 3000 escuchando...");
+});
